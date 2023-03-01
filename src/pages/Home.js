024200e95454df8aa46aae51455e305e81e2fab3
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-03-01 13:25:06 +0600

    show product data to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,20 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
+import ProductCard from "../components/ProductCard";
 
 const Home = () => {
-  const state = useSelector((state) => state);
-  console.log(state);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10">
-      <h1>This is home page</h1>
+      {products.map((product) => (
+        <ProductCard key={product._id} product={product} />
+      ))}
     </div>
   );
 };
