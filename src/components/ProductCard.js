de0319d2e62d64f7a54033128cfa4bf789897f2c
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-03-01 13:55:53 +0600

    when add to product button click then update the quantity of product to with 1 and show the quantity to product card in cart page as a badge

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -11,7 +11,12 @@ const ProductCard = ({ product }) => {
   const { pathname } = useLocation();
 
   return (
-    <div className="shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900">
+    <div className="relative shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900">
+      {pathname.includes("cart") && (
+        <div className="bg-indigo-500 h-8 w-8 grid place-content-center rounded-full absolute top-2 right-2">
+          <p className="text-white font-semibold">{product.quantity}</p>
+        </div>
+      )}
       <div className="h-52 w-52 mx-auto">
         <img src={product.image} alt={product.model} />
       </div>
--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -5,11 +5,25 @@ const initialState = {
 };
 
 export const productReducer = (state = initialState, action) => {
+  const selectedProduct = state.cart.find(
+    (product) => product._id === action.payload._id
+  );
+
   switch (action.type) {
     case ADD_TO_CART:
+      if (selectedProduct) {
+        selectedProduct.quantity = selectedProduct.quantity + 1;
+        const newCart = state.cart.filter(
+          (product) => product._id !== selectedProduct._id
+        );
+        return {
+          ...state,
+          cart: [...newCart, selectedProduct],
+        };
+      }
       return {
         ...state,
-        cart: [...state.cart, action.payload],
+        cart: [...state.cart, { ...action.payload, quantity: 1 }],
       };
     default:
       return state;
